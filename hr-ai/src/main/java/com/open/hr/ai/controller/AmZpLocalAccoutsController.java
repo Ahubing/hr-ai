7440590b787758378debdeef5524f74e5be15f90
jiuhao <5489486+lixinjiuhao@user.noreply.gitee.com>
2025-01-04 00:53:59 +0800

    新增ai招聘模块 db模块

--- hr-ai/src/main/java/com/open/hr/ai/controller/AmZpLocalAccoutsController.java
+++ hr-ai/src/main/java/com/open/hr/ai/controller/AmZpLocalAccoutsController.java
@@ -3,6 +3,10 @@ package com.open.hr.ai.controller;
 
 import com.open.ai.eros.common.vo.ResultVO;
 import com.open.ai.eros.db.mysql.hr.entity.AmZpLocalAccouts;
+import com.open.hr.ai.manager.AmZpLocalAccoutsManager;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Eros-AI
  * @since 2025-01-04
  */
+@Api(tags = "招聘本地账户前端控制器")
 @RestController
 @RequestMapping("/amZpLocalAccouts")
 public class AmZpLocalAccoutsController {
 
 
+    @Autowired
+    private AmZpLocalAccoutsManager amZpLocalAccoutsManager;
+
+
+    @ApiOperation("1获取本地登录账号列表")
     @GetMapping("/chatbot/get_local_accounts")
     public ResultVO<AmZpLocalAccouts> list(){
         return null;
