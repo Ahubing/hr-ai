eb72a7c95fea8a0ad3be8dca70f53ead06a0f23b
jiuhao <5489486+lixinjiuhao@user.noreply.gitee.com>
2025-01-04 11:47:09 +0800

    add login controller

---
+++ hr-ai/src/main/java/com/open/hr/ai/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.open.hr.ai.controller;
+
+import com.open.hr.ai.config.HrAIBaseController;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @类名：LoginController
+ * @项目名：ai-recruitment
+ * @description：
+ * @创建人：陈臣
+ * @创建时间：2025/1/4 11:46
+ */
+
+@RestController
+public class LoginController extends HrAIBaseController {
+
+
+
+
+
+}
