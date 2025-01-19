cf260196a17b4d5cd17b2796ba6440f2399b734c
linyous <liuzilin@lizhi.fm>
2025-01-20 00:01:07 +0800

    feat: 调整代码

---
+++ eros-db/src/main/java/com/open/ai/eros/db/mysql/hr/service/IAmMaskService.java
@@ -0,0 +1,16 @@
+package com.open.ai.eros.db.mysql.hr.service;
+
+import com.open.ai.eros.db.mysql.hr.entity.AmMask;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 面具 服务类
+ * </p>
+ *
+ * @author Eros-AI
+ * @since 2025-01-19
+ */
+public interface IAmMaskService extends IService<AmMask> {
+
+}
---
+++ eros-db/src/main/java/com/open/ai/eros/db/mysql/hr/service/impl/AmMaskServiceImpl.java
@@ -0,0 +1,20 @@
+package com.open.ai.eros.db.mysql.hr.service.impl;
+
+import com.open.ai.eros.db.mysql.hr.entity.AmMask;
+import com.open.ai.eros.db.mysql.hr.mapper.AmMaskMapper;
+import com.open.ai.eros.db.mysql.hr.service.IAmMaskService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 面具 服务实现类
+ * </p>
+ *
+ * @author Eros-AI
+ * @since 2025-01-19
+ */
+@Service
+public class AmMaskServiceImpl extends ServiceImpl<AmMaskMapper, AmMask> implements IAmMaskService {
+
+}
