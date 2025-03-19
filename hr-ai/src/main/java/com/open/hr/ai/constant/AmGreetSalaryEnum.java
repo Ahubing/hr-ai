b2027dddcc9240399febb53be7f02c5754e932ac
linyous <liuzilin@lizhi.fm>
2025-03-19 14:09:28 +0800

    feat: 打招呼任务枚举条件处理

--- hr-ai/src/main/java/com/open/hr/ai/constant/AmGreetSalaryEnum.java
+++ hr-ai/src/main/java/com/open/hr/ai/constant/AmGreetSalaryEnum.java
@@ -11,12 +11,12 @@ public enum AmGreetSalaryEnum {
      * 50k以上
      */
     UM_LIMITED("不限","不限"),
-    THREE_K_BELOW("3k","3k以下"),
+    THREE_K_BELOW("3k以下","3k以下"),
     THREE_TO_FIVE_K("3-5k","3-5k"),
     FIVE_TO_TEN_K("5-10k","5-10k"),
     TEN_TO_TWENTY_K("10-20k","10-20k"),
     TWENTY_TO_FIFTY_K("20-50k","20-50k"),
-    FIFTY_ABOVE("50k","50k以上")
+    FIFTY_ABOVE("50k以上","50k以上")
    ;
 
     private final String type;
