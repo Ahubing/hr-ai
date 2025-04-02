12fa009159cc273b12e1c364d8700e764f0d59be
hubing <614639641@qq.com>
2025-04-02 10:42:54 +0800

    modify:职位列表

--- hr-ai/src/main/java/com/open/hr/ai/bean/req/updatePositionReq.java
+++ hr-ai/src/main/java/com/open/hr/ai/bean/req/updatePositionReq.java
@@ -21,7 +21,7 @@ public class updatePositionReq {
     /**
      * 岗位描述
      */
-    @ApiModelProperty(value = "必填，岗位描述", notes = "岗位描述")
+    @ApiModelProperty(value = "岗位描述", notes = "岗位描述")
     private String  desc;
 
     @ApiModelProperty("招聘渠道/平台id")
